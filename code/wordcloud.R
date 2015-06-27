b3cafd40733b8308aab2c0841832857ab47330b1
vadimus202 <vadimus202@gmail.com>
2015-06-27 19:58:14 -0400

    update wordcloud

--- code/wordcloud.R
+++ code/wordcloud.R
@@ -1,3 +1,7 @@
+library(dplyr)
+
+load('data/clean/nash_restaurants.RData')
+
 
 # Normalize ratings
 nash <- mutate(nash,
@@ -17,17 +21,18 @@ nash <- mutate(nash,
 
 
 categ <-  data.frame(
-  cat = unlist(lapply(nash$categories, strsplit, split = ':')),
-  cat_cd = unlist(lapply(nash$categories_cd, strsplit, split = ':')),
-  stringsAsFactors = F) %>% 
-  unique() %>% 
-  arrange(rank(cat_cd))
+    cat = unlist(lapply(nash$categories, strsplit, split = ':')),
+    cat_cd = unlist(lapply(nash$categories_cd, strsplit, split = ':')),
+    stringsAsFactors = F) %>% 
+    unique() %>% 
+    filter(cat_cd!='restaurants') %>% 
+    arrange(rank(cat_cd))
 
 
 # Extract full name lookup for Yelp categories
 categ.unlist <- function(x) {
-  paste(unlist(lapply(x$categories_cd, strsplit, split = ':')),collapse = ' ')
-  }
+    paste(unlist(lapply(x$categories_cd, strsplit, split = ':')),collapse = ' ')
+}
 
 q1 <- categ.unlist(filter(nash, yelp_qrt==1))
 q2 <- categ.unlist(filter(nash, yelp_qrt==2))
@@ -38,9 +43,10 @@ all <- c(q1,q2,q3,q4)
 
 
 # Frequency of Yelp categories
-require(tm)
-require(wordcloud)
+library(tm)
+library(wordcloud)
 corpus <- Corpus(VectorSource(all))
+corpus <- tm_map(corpus, removeWords, 'restaurants')
 m <- as.matrix(TermDocumentMatrix(corpus))
 colnames(m) <- c('Worst 25%','25-50%','50-75%','Best 25%')
 rownames(m) <- categ$cat
@@ -49,8 +55,8 @@ rownames(m) <- categ$cat
 png('img/wordcloud.png',width = 900, height = 900, res = 72*3)
 set.seed(999)
 comparison.cloud(m, scale=c(2, 0.5), title.size=1,
-          random.order=FALSE, rot.per=0,  
-          use.r.layout=FALSE, colors=c('red','darksalmon','palegreen4','green4'))
+                 random.order=FALSE, rot.per=0,  
+                 use.r.layout=FALSE, colors=c('red','darksalmon','palegreen4','green4'))
 dev.off()
 shell.exec('img/wordcloud.png')
 
