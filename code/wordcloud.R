56d9bdbabc0e7ad11e56e04e8f6d561c06712601
Vadim B <vadimus202@gmail.com>
2015-07-07 14:57:04 -0400

    reorder wordcloud

--- code/wordcloud.R
+++ code/wordcloud.R
@@ -1,3 +1,5 @@
+library(tm)
+library(wordcloud)
 library(dplyr)
 
 load('data/clean/nash_restaurants.RData')
@@ -39,29 +41,28 @@ q2 <- categ.unlist(filter(nash, yelp_qrt==2))
 q3 <- categ.unlist(filter(nash, yelp_qrt==3))
 q4 <- categ.unlist(filter(nash, yelp_qrt==4))
 
-all <- c(q1,q2,q3,q4)
+# reorder for wordcloud
+all <- c(q3,q4,q2,q1)
 
 
 # Frequency of Yelp categories
-library(tm)
-library(wordcloud)
 corpus <- Corpus(VectorSource(all))
 corpus <- tm_map(corpus, removeWords, 'restaurants')
 m <- as.matrix(TermDocumentMatrix(corpus))
-colnames(m) <- c('Worst 25%','25-50%','50-75%','Best 25%')
+colnames(m) <- c('50-75%','Best 25%','25-50%','Worst 25%')
 rownames(m) <- categ$cat
 
 # print wordcloud:
 png('img/wordcloud.png',
-    width = 900, height = 800, 
+    width = 1000, height = 900, 
     res = 72*3, pointsize = 12, 
     bg = 'transparent')
-set.seed(999)
+set.seed(254)
 comparison.cloud(m, scale=c(2, 0.5), title.size=1,
                  random.order=FALSE, rot.per=0,  
-                 use.r.layout=FALSE, colors=c('red','darksalmon','palegreen4','green4'))
+                 use.r.layout=FALSE, colors=c('palegreen4','green4','darksalmon','red'))
 dev.off()
-shell.exec('img/wordcloud.png')
+shell.exec('img\\wordcloud.png')
 
 
 
