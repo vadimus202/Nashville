a3e00f21f4b735f32243e3abf7aa95207df485d5
vadimus202 <vadimus202@gmail.com>
2015-07-09 11:07:12 -0400

    add Uber info to cached data

--- code/03_Get_Data_Yelp_Hot_Chicken.R
+++ code/03_Get_Data_Yelp_Hot_Chicken.R
@@ -3,12 +3,17 @@ library(dplyr)
 load('data/clean/nash_restaurants.RData')
 
 source('code/function_Yelp_API_TermSearch.R')
+source('code/function_Uber_API.R')
+source('code/function_Bayes_Avg.R')
+
+# place uber token in global env
+source("../setup_uber_oauth.R")
+stopifnot(exists('uber_token'))
 
 # distance to AGA
-addr = '2800 Opryland Drive, Nashville, TN 37214'
-geo.loc <- ggmap::geocode(location = addr)
-geo.loc <- c(geo.loc$lat, geo.loc$lon)
-
+aga.addr = '2800 Opryland Drive, Nashville, TN 37214'
+aga.loc <- ggmap::geocode(location = aga.addr)
+stopifnot(length(aga.loc)==2)
 
 # call Yelp API
 hot.chic.yelp <- lkp_yelp_by_category(term = 'hot chicken', 
@@ -42,7 +47,6 @@ hot.chic <- hot.chic %>%
     filter(matches>1L)
 
 # add weighted ratings-based ranks
-source('code/function_Bayes_Avg.R')
 hot.chic <- hot.chic %>% 
     mutate(prc = rank(matches)/length(matches),
            adj_votes = review_count*prc) %>% 
@@ -59,6 +63,16 @@ hot.chic <- hot.chic %>%
     left_join(
         select(nash, id, neighborhood),
         by='id')
+
+# add Uber info
+df.uber <- hot.chic %>% 
+    mutate(lat1 = as.numeric(aga.loc$lat),
+           lon1 = as.numeric(aga.loc$lon)) %>% 
+    select(id, lat1, lon1, lat2=lat, lon2=lng)
+
+df.uber <- get_uber_estimates(df = df.uber, uber_token = uber_token)
+
+hot.chic <- left_join(hot.chic, df.uber, by='id')
 
 
 # cache results
