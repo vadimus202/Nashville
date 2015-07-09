f286ffa8818e9d285979610d1d2b274e8fbda7fc
vadimus202 <vadimus202@gmail.com>
2015-07-09 11:59:27 -0400

    add uber and Bayesian ranks to music venues

--- code/04_Get_Data_Yelp_Music.R
+++ code/04_Get_Data_Yelp_Music.R
@@ -1,12 +1,19 @@
 library(dplyr)
-library(geosphere)
 
+# source functions
+source("code/function_Uber_API.R")
 source('code/function_Yelp_API_TermSearch.R')
+source('code/function_Bayes_Avg.R')
+
+# place uber token in global env
+source("../setup_uber_oauth.R")
+stopifnot(exists('uber_token'))
+
 
 # distance to AGA
-addr = '2800 Opryland Drive, Nashville, TN 37214'
-geo.loc <- ggmap::geocode(location = addr)
-geo.loc <- c(geo.loc$lat, geo.loc$lon)
+aga.addr = '2800 Opryland Drive, Nashville, TN 37214'
+aga.loc <- ggmap::geocode(location = aga.addr)
+stopifnot(length(aga.loc)==2)
 
 
 # call Yelp API
@@ -18,14 +25,21 @@ music.yelp <- lkp_yelp_by_category(term = '',
 # review results
 music.yelp$errors
 music.yelp$elapsed.min
-nash.music <- music.yelp$results %>% 
-  mutate(dist =  distCosine(geo.loc, 
-                            cbind(lat, lng), 
-                            r = 6378137*0.000621371192),
-         dist = round(dist,2),
-         rank = rating*review_count,
-         rank = as.integer(rank(-rank))) %>% 
-  arrange(rank)
+nash.music <- music.yelp$results 
+
+nash.music <- get_bayes_avg(nash.music, min_votes = 35)
+
+# add Uber info
+df.uber <- nash.music %>% 
+    mutate(lat1 = as.numeric(aga.loc$lat),
+           lon1 = as.numeric(aga.loc$lon)) %>% 
+    select(id, lat1, lon1, lat2=lat, lon2=lng)
+
+df.uber <- get_uber_estimates(df = df.uber, uber_token = uber_token)
+
+nash.music <- left_join(nash.music, df.uber, by='id')
+
+
 
 
 # cache results
